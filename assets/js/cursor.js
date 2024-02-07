a9fdca003daf03bec7ba35feb86afea1c0674810
BohdanMK <b.m.khristevich@gmail.com>
2024-02-07 11:55:53 +0200

    add last result

--- assets/js/cursor.js
+++ assets/js/cursor.js
@@ -2,35 +2,42 @@
 console.log('1231231');
 
 $(document).ready(function(){
+
+  var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+  if(windowWidth > 991) {
     $("body").on("mousemove", function (n) {
-    a.css({'left' : n.clientX + "px" });
-    a.css({'top' : n.clientY + "px" });
-    b.css({'left' : n.clientX + "px" });
-    b.css({'top' : n.clientY + "px" });
-    c.css({'left' : n.clientX + "px" });
-    c.css({'top' : n.clientY + "px" });
-  });
+      a.css({'left' : n.clientX + "px" });
+      a.css({'top' : n.clientY + "px" });
+      b.css({'left' : n.clientX + "px" });
+      b.css({'top' : n.clientY + "px" });
+      c.css({'left' : n.clientX + "px" });
+      c.css({'top' : n.clientY + "px" });
+    });
 
-  var a = $("#cursor"),
-      b = $("#cursor2"),
-      c = $("#cursor3");
+    var a = $("#cursor"),
+        b = $("#cursor2"),
+        c = $("#cursor3");
 
-  function n(t) {
-    b.addClass("hover");
-    c.addClass("hover");
+    function n(t) {
+      b.addClass("hover");
+      c.addClass("hover");
+    }
+
+    function s(t) {
+      b.removeClass("hover");
+      c.removeClass("hover");
+    }
+
+    s();
+
+    $("a").on('mouseover', n );
+    $("a").on('mouseout', s );
+    $("button").on('mouseover', n );
+    $("button").on('mouseout', s );
   }
 
-  function s(t) {
-    b.removeClass("hover");
-    c.removeClass("hover");
-  }
 
-  s();
-
-  $("a").on('mouseover', n );
-  $("a").on('mouseout', s );
-  $("button").on('mouseover', n );
-  $("button").on('mouseout', s );
 
 })
 
--- assets/js/glitch.js
+++ assets/js/glitch.js
@@ -1,75 +1,80 @@
-function createGlitchEffect(elementId) {
-    let bg = document.getElementById(elementId);
-    let intervalId;
-    let count = 240;
-    let interval = false;
+var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
-    function showGlitch() {
-        console.log(interval);
-        if (interval) {
+if(windowWidth > 991) {
+    function createGlitchEffect(elementId) {
+        let bg = document.getElementById(elementId);
+        let intervalId;
+        let count = 240;
+        let interval = false;
+
+        function showGlitch() {
             console.log(interval);
-            return;
-        }
-
-        interval = true;
-
-        for (let i = 0; i < count; i++) {
-            let glitchBox = document.createElement('div');
-            glitchBox.className = 'box';
-            bg.appendChild(glitchBox);
-        }
-
-        let iterationCount = 0;
-
-        intervalId = setInterval(function () {
-            let glitch = document.getElementsByClassName('box');
-
-            for (let i = 0; i < glitch.length; i++) {
-                glitch[i].style.marginLeft = Math.floor(Math.random() * 45) + 'px';
-                console.log(glitch[i].style.left);
-                glitch[i].style.marginTop = Math.floor(Math.random() * 15) + 'px';
-                glitch[i].style.width = Math.floor(60) + 'px';
-                glitch[i].style.height = Math.floor(15) + 'px';
+            if (interval) {
+                console.log(interval);
+                return;
             }
 
-            iterationCount++;
+            interval = true;
 
-            if (iterationCount === 16) {
-                clearInterval(intervalId);
-                interval = false;
-                stopGlitch();
+            for (let i = 0; i < count; i++) {
+                let glitchBox = document.createElement('div');
+                glitchBox.className = 'box';
+                bg.appendChild(glitchBox);
             }
-        }, 30);
+
+            let iterationCount = 0;
+
+            intervalId = setInterval(function () {
+                let glitch = document.getElementsByClassName('box');
+
+                for (let i = 0; i < glitch.length; i++) {
+                    glitch[i].style.marginLeft = Math.floor(Math.random() * 45) + 'px';
+                    console.log(glitch[i].style.left);
+                    glitch[i].style.marginTop = Math.floor(Math.random() * 15) + 'px';
+                    glitch[i].style.width = Math.floor(60) + 'px';
+                    glitch[i].style.height = Math.floor(15) + 'px';
+                }
+
+                iterationCount++;
+
+                if (iterationCount === 16) {
+                    clearInterval(intervalId);
+                    interval = false;
+                    stopGlitch();
+                }
+            }, 30);
+        }
+
+        function stopGlitch() {
+            bg.innerHTML = '';
+        }
+
+        // Отримайте елемент "page"
+        var pageElement = document.getElementById(elementId);
+        console.log(pageElement);
+
+        // Перевірте, чи елемент існує перед додаванням обробників подій
+        if (pageElement) {
+            // Додайте обробник подій для події "mouseenter" (наведення миші)
+            pageElement.addEventListener('mouseenter', showGlitch);
+
+            // Додайте обробник подій для події "mouseleave" (виведення миші)
+            // pageElement.addEventListener('mouseleave', stopGlitch);
+        } else {
+            console.error('Element with id "' + elementId + '" not found.');
+        }
+
+        setInterval(function () {
+            let randomDelay = Math.floor(Math.random() * 10000);
+            console.log();
+
+            setTimeout(showGlitch, randomDelay);
+        }, 10000);
     }
 
-    function stopGlitch() {
-        bg.innerHTML = '';
-    }
-
-    // Отримайте елемент "page"
-    var pageElement = document.getElementById(elementId);
-    console.log(pageElement);
-
-    // Перевірте, чи елемент існує перед додаванням обробників подій
-    if (pageElement) {
-        // Додайте обробник подій для події "mouseenter" (наведення миші)
-        pageElement.addEventListener('mouseenter', showGlitch);
-
-        // Додайте обробник подій для події "mouseleave" (виведення миші)
-        // pageElement.addEventListener('mouseleave', stopGlitch);
-    } else {
-        console.error('Element with id "' + elementId + '" not found.');
-    }
-
-    setInterval(function () {
-        let randomDelay = Math.floor(Math.random() * 10000);
-        console.log();
-
-        setTimeout(showGlitch, randomDelay);
-    }, 10000);
+    // Виклик функції для елементу з id "glitch"
+    createGlitchEffect('glitch1');
+    createGlitchEffect('glitch2');
+    createGlitchEffect('glitch3');
 }
 
-// Виклик функції для елементу з id "glitch"
-createGlitchEffect('glitch1');
-createGlitchEffect('glitch2');
-createGlitchEffect('glitch3');
